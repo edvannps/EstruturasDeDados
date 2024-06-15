554b15a5f07945c091f9b293341810721ad70a64
Edvan Sabino <edvansabino@gmail.com>
2024-06-15 11:08:23 -0300

    Pop method implementation

--- src/main/java/dataStructure/Stack.java
+++ src/main/java/dataStructure/Stack.java
@@ -53,17 +53,31 @@ public class Stack {
         height++;
     }
 
+    public Node pop() {
+        if (height == 0) return null;
+
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+
+        return temp;
+    }
+
     public static void main(String[] args) {
         Stack myStack = new Stack(2);
-
-        myStack.getTop();
-        myStack.getHeight();
-        myStack.print();
-
         myStack.push(1);
 
-        myStack.getTop();
-        myStack.getHeight();
-        myStack.print();
+        System.out.println(myStack.pop().value);
+        System.out.println(myStack.pop().value);
+        System.out.println(myStack.pop() == null);
+
+//        myStack.getTop();
+//        myStack.getHeight();
+//        myStack.print();
+//
+//        myStack.getTop();
+//        myStack.getHeight();
+//        myStack.print();
     }
 }
