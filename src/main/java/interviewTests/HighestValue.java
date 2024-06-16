1b1da73dd168bac5e424a2393241acd2551e83c3
Edvan Sabino <edvansabino@gmail.com>
2024-06-15 21:29:29 -0300

    Highest value level tree problem

---
+++ src/main/java/interviewTests/HighestValue.java
@@ -0,0 +1,42 @@
+package interviewTests;
+
+import dataStructure.Tree;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HighestValue {
+
+    public static List<Integer> largestValue(Tree tree) {
+        var list = new ArrayList<Integer>();
+        solveTree(tree.root, 0, list);
+
+        return list;
+    }
+
+    private static void solveTree(final Tree.Node node, final int level, final ArrayList<Integer> list) {
+
+        if (node == null) return;
+
+        if (level == list.size()) {
+            list.add(node.value);
+        } else {
+            list.set(level, Math.max(list.get(level), node.value ));
+        }
+
+        solveTree(node.left, level+1, list);
+        solveTree(node.right, level+1, list);
+    }
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert(100);
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(6);
+        tree.insert(1);
+        tree.insert(7);
+        tree.insert(5);
+
+        System.out.println(largestValue(tree));
+    }
+}
